11eb1a3436723ed731c13c19195119b82285fb23
Aurélie Grebe <aureliegrebe@gmail.com>
2023-01-22 12:22:59 -0500

    Functionnal day 2 (both stars)

--- day_02/src/main.rs
+++ day_02/src/main.rs
@@ -5,25 +5,65 @@ use std::path::Path;
 static FILE_PATH: &str = "./data/input";
 
 fn main() {
-
-    let mut total_score: i32 = 0;
+    let mut total_score_part_1: i32 = 0;
+    let mut total_score_part_2: i32 = 0;
 
     // iterate over data
     if let Ok(lines) = read_lines(FILE_PATH) {
         for line in lines {
             if let Ok(line_str) = line {
-                total_score += score_round(&line_str);
+                total_score_part_1 += score_round_part_1(&line_str);
+                total_score_part_2 += score_round_part_2(&line_str);
             }
         }
-
     }
 
-    println!("Total score: {}", total_score)
-
+    println!("Total score (part 1): {}", total_score_part_1);
+    println!("Total score (part 2): {}", total_score_part_2);
 }
 
 /// Calculate the score for a single round of rock, paper, scissors
-fn score_round(round_str: &str) -> i32 {
+fn score_round_part_2(round_str: &str) -> i32 {
+    let opponent_play: char = round_str.as_bytes()[0] as char;
+    let my_play: char = round_str.as_bytes()[2] as char;
+
+    match my_play {
+        // player loses
+        'X' => match opponent_play {
+            // rock - scissor
+            'A' => 3,
+            // paper - rock
+            'B' => 1,
+            // scissors - paper
+            'C' => 2,
+            _ => panic!("Invalid input (opponent)"),
+        },
+        // player draws
+        'Y' => match opponent_play {
+            // rock - rock
+            'A' => 4,
+            // paper - paper
+            'B' => 5,
+            // scissors - scissors
+            'C' => 6,
+            _ => panic!("Invalid input (opponent)"),
+        },
+        // player wins
+        'Z' => match opponent_play {
+            // rock - paper
+            'A' => 8,
+            // paper - scissors
+            'B' => 9,
+            // scissors - rock
+            'C' => 7,
+            _ => panic!("Invalid input (opponent)"),
+        },
+        _ => panic!("Invalid input (player)"),
+    }
+}
+
+/// Calculate the score for a single round of rock, paper, scissors
+fn score_round_part_1(round_str: &str) -> i32 {
     let opponent_play: char = round_str.as_bytes()[0] as char;
     let my_play: char = round_str.as_bytes()[2] as char;
 
@@ -36,7 +76,7 @@ fn score_round(round_str: &str) -> i32 {
             'B' => 1,
             // scissors - win
             'C' => 7,
-            _ => panic!("Invalid input (opponent)"), 
+            _ => panic!("Invalid input (opponent)"),
         },
         // paper - 2 points
         'Y' => match opponent_play {
@@ -46,7 +86,7 @@ fn score_round(round_str: &str) -> i32 {
             'B' => 5,
             // scissors - loss
             'C' => 2,
-            _ => panic!("Invalid input (opponent)"), 
+            _ => panic!("Invalid input (opponent)"),
         },
         //scissors - 3 points
         'Z' => match opponent_play {
@@ -56,19 +96,16 @@ fn score_round(round_str: &str) -> i32 {
             'B' => 9,
             // scissors - draw
             'C' => 6,
-            _ => panic!("Invalid input (opponent)"), 
+            _ => panic!("Invalid input (opponent)"),
         },
-        _ => panic!("Invalid input (player)"), 
+        _ => panic!("Invalid input (player)"),
     }
-
-
-
 }
 
 fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
 where
     P: AsRef<Path>,
 {
-    let file = File:: open(filename)?;
+    let file = File::open(filename)?;
     Ok(io::BufReader::new(file).lines())
 }
